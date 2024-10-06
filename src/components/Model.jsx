185ebc5e2a75964873bcf5b3d4312ae50df9c774
Divyansh Gusain <divyanshdh@gmail.com>
2024-10-06 14:41:45 +0530

    Fixed color picker of iphone model design and making it float over the model section

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -47,11 +47,54 @@ const Model = () => {
     }
   });
 
+  const parentRef = useRef(null);
+  const divRef = useRef(null);
+
+  
+  useEffect(() => {
+    gsap.fromTo(
+      divRef.current, 
+      { opacity: 0.9, y: 0, visibility: "hidden", position: "fixed" }, 
+      {
+        opacity: 1,
+        y: 0,
+        visibility: "visible",
+        duration: 0.5,
+        scrollTrigger: {
+          trigger: parentRef.current, 
+          start: "top bottom",  
+          end: "bottom top",  
+          toggleActions: "play none none reverse",
+          scrub: true,  
+          onEnter: () => {
+            gsap.to(divRef.current, {
+              opacity: 1,
+              visibility: "visible",
+              y: 0,
+              position: "sticky",
+              bottom: 20,  
+              duration: 0.5
+            });
+          },
+          onLeaveBack: () => {
+            gsap.to(divRef.current, {
+              position: "fixed",
+              bottom: 20,
+              duration: 0.5,
+              opacity:1
+            });
+          },
+        }
+      }
+    );
+  }, []);
+  
+
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
   return (
-    <section className="common-padding">
+    <section className="common-padding" ref={parentRef}>
       <div className="screen-max-width">
         <h1 id="heading" className="section-heading" style={{ color: "white" }}>
           Take a closer look.
@@ -94,17 +137,21 @@ const Model = () => {
             </Canvas>
           </div>
 
-          <div className="mx-auto w-full">
-            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+          <div
+            className="mx-auto left-1 right-1 z-10 mt-10 overflow-visible w-fit"
+            ref={divRef}
+            style={{ bottom: "20px" }} 
+          >
+            <p className="text-sm font-light text-center mb-5 bg-gray-300 w-fit p-2 rounded-full">
+              {model.title}
+            </p>
             <div className="flex-center">
               <ul className="color-container">
                 {models.map((item, i) => (
                   <li
                     key={i}
-                    className="w-6 h-6 rounded-full mx-2 border-1px  cursor-pointer border-2 border-sky-50"
-                    style={{
-                      backgroundColor: item.color[0],
-                    }}
+                    className="w-6 h-6 rounded-full mx-2 border-1px cursor-pointer border-2 border-sky-50"
+                    style={{ backgroundColor: item.color[0] }}
                     onClick={() => setModel(item)}
                   />
                 ))}
