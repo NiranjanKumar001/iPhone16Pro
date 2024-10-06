5c80272db9cdeec5c9b7e7b183633f7ccf28a63f
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-10-06 21:15:58 +0530

    Creating Pages + Including Navbar and Footer

---
+++ src/components/pages/AirPods.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../layouts/Navbar'
+import Footer from '../Footer'
+
+const AirPods = () => {
+  return (
+    <div>
+      <Navbar />
+      AirPods
+      <Footer />
+
+    </div>
+  )
+}
+
+export default AirPods
---
+++ src/components/pages/SiteMap.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../layouts/Navbar'
+import Footer from '../Footer'
+
+const SiteMap = () => {
+  return (
+    <div>
+      <Navbar/>
+      SiteMap
+      <Footer/>
+    </div>
+  )
+}
+
+export default SiteMap
