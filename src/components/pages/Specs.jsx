fa1ccdce36c1d1246d61fa717b0849a99b2e76f1
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-10-07 08:46:17 +0530

    Creating Pages and Adding ( Navbar + FloatingNavbar + Footer)

---
+++ src/components/pages/Specs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../layouts/Navbar'
+import Footer from '../Footer'
+import FloatingNavbar from '../FloatingNavbar'
+
+const Specs = () => {
+  return (
+    <div>
+      <Navbar />
+      <FloatingNavbar />
+      Specs
+      <Footer />
+    </div>
+  )
+}
+
+export default Specs
