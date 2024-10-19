b2b24c9d5e1bb3a7d8763d7c7a5ddbf6c9d19919
Kruthik S <gokruthik2003@gmail.com>
2024-10-19 22:04:40 +0530

    Completed building the section

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 // import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Model from "./components/Model"
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import Features from "./components/Features";
-import HowItWorks from "./components/HowItWorks"
+import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
- 
-import * as Sentry from '@sentry/react';
 
-const App =() => {
+import * as Sentry from "@sentry/react";
+import AppleIntelligence from "./components/AppleIntelligence";
 
+const App = () => {
   // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
 
   return (
     <main className="bg-black">
       <Hero />
-      <Highlights/>
-      <Model/>
-      <Features/>
-      <HowItWorks/>
-      <Footer/>
-  </main>
-  )
-}
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <AppleIntelligence />
+      <Footer />
+    </main>
+  );
+};
 
 export default Sentry.withProfiler(App);
